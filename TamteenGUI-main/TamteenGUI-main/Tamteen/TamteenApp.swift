0a0fb2f78892583a9c864c8f8886765c4fbdbec0
Nora Basalamah <nbasalamah@sami.com.sa>
2025-12-12 10:24:33 +0300

    add UI  code

---
+++ TamteenGUI-main/TamteenGUI-main/Tamteen/TamteenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamteenApp.swift
+//  Tamteen
+//
+//  Created by Nouf AlShalhoub on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TamteenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TamteenGUI-main/TamteenGUI-main/TamteenTests/TamteenTests.swift
@@ -0,0 +1,17 @@
+//
+//  TamteenTests.swift
+//  TamteenTests
+//
+//  Created by Nouf AlShalhoub on 11/12/2025.
+//
+
+import Testing
+@testable import Tamteen
+
+struct TamteenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
